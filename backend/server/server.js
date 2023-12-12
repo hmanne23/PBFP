16dbdc0d3f10c5d056ea05b2c29267f245323599
hmanne23 <143413901+hmanne23@users.noreply.github.com>
2023-12-11 23:05:26 -0500

    final

--- backend/server/server.js
+++ backend/server/server.js
@@ -20,7 +20,7 @@ const corsOptions = {
   optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.use(cors(corsOptions));
+app.use(cors({ origin: '*' }));
 
 
 // Replace 'your-database-name' with your actual database name
